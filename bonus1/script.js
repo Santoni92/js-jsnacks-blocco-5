c2b2660bbb8cecefe6714086ddb039a3bd60433b
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-22 17:44:44 +0100

    funzione prende in input i due array e gli fonda aggiungendo prima un elemento del primo e poi,nella stessa posizione, un elemento del secondo array

---
+++ bonus1/script.js
@@ -0,0 +1,19 @@
+console.log('JS OK!');
+
+/*Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da
+ uno e dall’altro, es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
+
+ const array1 = ['a','b','c'];
+ const array2 = [1,2,3];
+fondaArray(array1,array2);
+
+ function fondaArray(array1,array2)
+ {
+     const unionArry = [];
+     for(let i = 0; i < array1.length; i++)
+     {
+         unionArry.push(array1[i]);
+         unionArry.push(array2[i]);
+     }
+     console.log(unionArry.join(' '));
+ }

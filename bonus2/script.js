fadcf139a0e8292ffdc68b1d6b67e99a390677a9
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-22 18:20:52 +0100

    bonus 2 ; aggiunta funzione che ritorna l'array degli elementi compresi nell'intervallo

---
+++ bonus2/script.js
@@ -0,0 +1,27 @@
+console.log('JS OK!');
+
+/*Scrivi una funzione che accetti tre argomenti: un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo 
+quanto il numero di elementi dell’array). La funzione ritornerà un nuovo array con i valori che hanno la posizione 
+compresa tra “a” e “b” */
+
+let a = parseInt(prompt('Inserisci estremo inferiore: '));
+let b = parseInt(prompt('Inserisci estremo superiore'));
+const array = [1,2,3,4,5,6,7,8,9,10];
+while((isNaN(a) && isNaN(b)) || a > b  || b >= array.length)
+{
+    a = parseInt(prompt('Inserisci estremo inferiore: '));
+    b = parseInt(prompt('Inserisci estremo superiore'));
+}
+
+console.log("L'array dei valori compresi nell'intervallo specificato risulta: " + valoriCompresi(a,b,array).join(' '));
+
+/*funzione */
+function valoriCompresi(number1,number2,array)
+{
+    const arrayToReturn = [];
+    for( let i = number1;i < number2; i++ )
+    {
+        arrayToReturn.push(array[i]);
+    }
+    return arrayToReturn;
+}
